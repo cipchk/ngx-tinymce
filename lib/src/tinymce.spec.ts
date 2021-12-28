64eb4d1a4a6db4552236e3406017df02feedf942
cipchk <cipchk@qq.com>
2021-12-28 16:30:02 +0800

    chore: fix test

--- lib/src/tinymce.spec.ts
+++ lib/src/tinymce.spec.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NgxTinymceModule } from '../index';
+import { NgxTinymceModule } from './tinymce.module';
 
 const html = ``;
 
